2ca5748ea564a654b0322f39ecb364c818e5730c
koth1999 <83741577+koth1999@users.noreply.github.com>
2023-06-14 00:46:35 +0900

    SignUp(미완성)
    SignUp(아직 미완성)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './pages/taehoon/SignUp';
+import Login from './pages/taehoon/Login';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/taehoon/Login.js
+++ src/pages/taehoon/Login.js
@@ -1,1 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const LoginBlock = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1200px;
+    height: 800px;
+`;
+
+const Login = () => {
+
+
+
+
+    return(
+        <LoginBlock>
+
+        </LoginBlock>
+    );
+}
+
+export default Login;
