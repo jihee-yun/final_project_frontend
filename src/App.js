5531d5cd89039906c5b07f354d2f06abf595b3da
jihee-yun <dandelion1141@gmail.com>
2023-06-14 21:22:07 +0900

    6.14일 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import SignUp from './pages/taehoon/SignUp';
 import Login from './pages/taehoon/Login';
+import CafeMain from './pages/jihee/CafeMain';
+import GuildMain from './pages/jihee/GuildMain';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/cafe" element={<CafeMain/>}/>
+        <Route path="/guild" element={<GuildMain/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/jihee/GuildMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const GuildMain = () => {
+  return(
+    <>
+    </>
+  );
+};
+
+export default GuildMain;
