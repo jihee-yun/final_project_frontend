fe377d137496643b6c3b02bd136ce977f3135813
yeab0824@naver.com <http://github.com/yeab0824>
2023-06-16 21:37:38 +0900

    230616

--- src/pages/yeabin/Roulette.js
+++ src/pages/yeabin/Roulette.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 
 
 const Roulette = () => {
+  const [selectItem, setSelectItem] = useState(null);
+  const itmes = ['1', '2', '3', '4', '5'];
+
+  const spinRoulette = () => {
+    const random = Math.floor(Math.random() * itmes.length);
+    setSelectItem(itmes[random]);
+  };
+
   return(
     <>
-
+    <div>
+      <h3>룰렛</h3>
+      {selectItem && <p>당첨 : {selectItem}</p>}
+      {/* <ul>
+        {itmes.map()}
+      </ul> */}
+    </div>
     </>
   );
 };
