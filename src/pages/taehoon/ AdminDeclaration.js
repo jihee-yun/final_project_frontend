d1694f49e59a160b997b1737b8f409ef6e43d012
koth1999 <83741577+koth1999@users.noreply.github.com>
2023-06-20 00:48:48 +0900

    230620 페이지 수정
    230620 페이지 수정

---
+++ src/pages/taehoon/ AdminDeclaration.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const AdminDeclaration = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+export default AdminDeclaration;
--- src/pages/taehoon/AdminReg.js
+++ src/pages/taehoon/AdminReg.js
@@ -2,20 +2,167 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import admin from "../../images/admin.png";
 
 const AdminRegBlock = styled.div`
     position: relative;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
+    margin-top: 100px;
+    color: #FFCFDA;
+    font-weight: bolder;
+    font-size: 30px;
+
+    .inputContainer {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .regBtn button{
+        width: 150px;
+        height: 30px;
+        margin-top: 80px;
+        border: none;
+        background-color: #FFCFDA;
+    }
+
+    .admin img {
+        width: 60px;
+        height: 60px;
+        margin-right: 350px;
+        margin-top : 10px;
+    }
+
+    h2 {
+        margin-bottom: -70px;
+    }
+
+    @media (max-width: 768px) {
+        /* 모바일 화면에 대한 스타일 조정 */
+        margin-top: 80px;
+        font-size: 20px;
+
+        .inputContainer {
+            margin-top: 40px;
+        }
+
+        .regBtn button {
+            width: 150px;
+            height: 30px;
+            margin-top: 60px;
+        }
+
+        .admin img {
+            margin-bottom: -10px;
+        }
+    }
 `;
 
-const AdminReg = () => {
-    return(
-        <AdminRegBlock>
-            <p>관리자 등록</p>
-        </AdminRegBlock>
-    );
-}
+const Input = styled.input`
+    margin-top: 10px;
+    width: 400px;
+    height: 25px;
+    padding: 10px;
+    border: 1px solid #999;
+    border-radius: 20px;
+    outline: none;
+`;
 
-export default AdminReg;
+const Select = styled.select`
+    margin-top: -45px;
+    width: 250px;
+    height: 50px;
+    padding: 10px;
+    border: 1px solid #999;
+    border-radius: 20px;
+    outline: none;
+    background-color: #EFEFEF;
+    font-size: 16px;
+
+    @media (max-width: 768px) {
+        width: 250px;
+        height: 50px;
+        margin-right: -20px;
+        margin-top : -40px;
+        margin-bottom: -20px;
+    }
+`;
+
+const Label = styled.div`
+    margin-right: 350px;
+    margin-top: 20px;
+`
+
+const AdminReg = () => {
+    const navigate = useNavigate("");
+
+    const [adminID, setAdminID] = useState('');
+    const [adminPw, setAdminPw] = useState('');
+    const [adminName, setAdminName] = useState('');
+    const [adminAge, setAdminAge] = useState('');
+  
+    const onChangeId = (e) => {
+      setAdminID(e.target.value);
+    };
+  
+    const onChangePw = (e) => {
+      setAdminPw(e.target.value);
+    };
+  
+    const onChangeName = (e) => {
+      setAdminName(e.target.value);
+    };
+
+    const onChangeAge = (e) => {
+        setAdminAge(e.target.value);
+    }
+
+    const handleClick = () => {
+        navigate('/admin');
+    }
+  
+
+  
+    return (
+      <AdminRegBlock>
+        <h2>관리자 등록</h2>
+
+        <div className="admin">
+            <img src={admin} alt="admin" className="admin"/>
+        </div>
+  
+        <div className="inputContainer">
+          <Input type="name" placeholder="이름" value={adminName} onChange={onChangeName} />
+        </div>
+
+        <div className="inputContainer">
+            <Label>성별 : </Label>
+                <Select>
+                    <option value="male">남성</option>
+                    <option value="female">여성</option>
+                </Select>
+        </div>
+
+        <div className="inputContainer">
+          <Input type="age" placeholder="나이" value={adminAge} onChange={onChangeAge} />
+        </div>
+
+        <div className="inputContainer">
+          <Input type="id" placeholder="아이디" value={adminID} onChange={onChangeId} />
+        </div>
+  
+        <div className="inputContainer">
+          <Input type="password" placeholder="비밀번호" value={adminPw} onChange={onChangePw} />
+        </div>
+  
+        <div className="regBtn">
+          <button onClick={handleClick}>등록</button>
+        </div>
+      </AdminRegBlock>
+    );
+  };
+  
+  export default AdminReg;
