1e9a610d281102b0296616586264dc463b0e95d8
yeab0824@naver.com <http://github.com/yeab0824>
2023-06-21 20:55:48 +0900

    20230621

--- src/pages/yeabin/ChallengeMain.js
+++ src/pages/yeabin/ChallengeMain.js
@@ -1,9 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+// import challengePin from "./images/challengePin.png"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Title = styled.div`
+  /* img {
+    display: flex;
+    width: 40px;
+    height: 40px;
+  } */
+ 
+  h3 {
+    display: flex;
+    font-weight: bolder;
+    margin-left: 10px;
+    width: 300px;
+  }
+`;
 
 const ChallengeMain = () => {
   return(
-    <>챌린지 메인</>
+    <>
+    <Container>
+      <Title>
+        <div>
+          {/* <img src={challengePin} alt="핀" /> */}
+          <h3>제휴 카페 5개 방문하기 챌린지</h3>
+        </div>
+      </Title>
+      
+    </Container>
+    </>
   );
 };
 
--- src/pages/yeabin/Event.js
+++ src/pages/yeabin/Event.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Roulette from "./Roulette";
+import quiz from "./images/quiz.png"
 
 const ContainerBox = styled.div`
   max-width: 1440px;
@@ -14,18 +15,70 @@ const Box = styled.div`
 
 const ChallengeBox = styled.div`
   /* background-color: #FFCFDA; */
-  border: 1px solid #FFCFDA;
   margin-top: 50px;
   width: 200px;
   height: 300px;
   display: inline-block;
   margin-right: 50px; 
+  border-radius: 20px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
 `;
 
 const QuizBox = styled.div`
-  h3{
-    
+  width: 280px;
+  height: 280px;
+  border-radius: 20px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  margin: 50px;
+
+  img {
+    width: 80px;
+    height: 80px;
+    margin: 20px 0 10px 20px;
   }
+  
+  h3 {
+    margin-left: 20px;
+    line-height: 10%;
+  }
+
+  p {
+    margin-left: 20px;
+    color: gray;
+    font-size: 1rem;
+  }
+
+  .quizButton{
+    width: 50%;
+    height: 50px;
+    margin: 0 auto;
+  }
+  button {
+    margin-top: 25px;
+    width: 100%;
+    height: 35px;
+    border: none;
+    border-radius: 20px;
+    color: white;
+    background-color: #7D5A5A;
+    font-size: 1.1rem;
+    font-weight: bolder;
+  }
+`;
+
+const AdBox = styled.div`
+  width: 280px;
+  height: 280px;
+  border: 1px solid black;
+  margin: 50px;
+`;
+
+const LuckyBox = styled.div`
+  width: 280px;
+  height: 280px;
+  border-radius: 20px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  margin: 50px;
 `;
 
 const Event = () => {
@@ -52,8 +105,23 @@ const Event = () => {
           </ChallengeBox>
           <Roulette />
           <QuizBox>
-            <h3>퀴즈</h3>
+            <div>
+              <img src={quiz} alt="퀴즈" />
+              <h3 className="quizTitle">깜짝 퀴즈</h3>
+              <p>퀴즈 풀고 포인트 받기</p>
+              <div className="quizButton">
+              <button>퀴즈 풀기</button>
+              </div>
+            </div>
           </QuizBox>
+          <AdBox>
+            <div><p>광고 이미지</p></div>
+          </AdBox>
+          <LuckyBox>
+            <div>
+              
+            </div>
+          </LuckyBox>
         </Box>
       </ContainerBox>
     </>
