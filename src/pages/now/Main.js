73fd1d968c319a3d81635bc2934bae722cde63b9
simnow <gusa_918@naver.com>
2023-06-23 17:21:40 +0900

    추가

--- src/pages/now/Main.js
+++ src/pages/now/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Map from "./component/Map";
 import Ranking from "./component/Ranking";
-import Recommend from "./component/"
+import Recommend from "./component/Recommend"
 import { styled } from "styled-components";
 
 const Container = styled.div`
