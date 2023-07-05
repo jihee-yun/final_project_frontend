06064fdb09b9c9d2bd4fbd3408ab68a470f9cc8b
JAEHYUNGKWAK <126457219+JAEHYUNGKWAK@users.noreply.github.com>
2023-07-05 21:57:49 +0900

    긴급수정2

--- src/pages/jaehyung/api/AxiosApi.js
+++ src/pages/jaehyung/api/AxiosApi.js
@@ -23,27 +23,27 @@ const AxiosApi = {
 
 
 
-  memberLogin: async(memberId, password) => {
-    const loginData = {
-        memberid: memberId,
-        password: password,
-      };
+  // memberLogin: async(memberId, password) => {
+  //   const loginData = {
+  //       memberid: memberId,
+  //       password: password,
+  //     };
     
-      try {
-        const response = await axios.post(KH_DOMAIN +'/member/login', loginData);
-        const { authToken } = response.data; // 서버에서 발급한 토큰 받아오기
+  //     try {
+  //       const response = await axios.post(KH_DOMAIN +'/member/login', loginData);
+  //       const { authToken } = response.data; // 서버에서 발급한 토큰 받아오기
     
-        // 토큰 저장 (로컬 스토리지 등에 저장)
-        localStorage.setItem('authToken', authToken);
+  //       // 토큰 저장 (로컬 스토리지 등에 저장)
+  //       localStorage.setItem('authToken', authToken);
     
-        // 이후에 API 요청 시 헤더에 토큰 포함하여 보내기
-        axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
+  //       // 이후에 API 요청 시 헤더에 토큰 포함하여 보내기
+  //       axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
     
-        return response.data; // 로그인 성공 시 추가 작업을 위해 필요한 데이터 반환
-      } catch (error) {
-        // 로그인 실패 처리
-        throw new Error('로그인에 실패했습니다.');
-      }
-    }
+  //       return response.data; // 로그인 성공 시 추가 작업을 위해 필요한 데이터 반환
+  //     } catch (error) {
+  //       // 로그인 실패 처리
+  //       throw new Error('로그인에 실패했습니다.');
+  //     }
+  //   }
 };
 export default AxiosApi;
